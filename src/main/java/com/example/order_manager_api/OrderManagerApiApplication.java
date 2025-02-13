61c31b388bb5b75b04f937bcbe600540b1e91281
aleolivera <aleoliverarp10@gmail.com>
2025-02-13 18:52:00 -0300

    first commit

---
+++ src/main/java/com/example/order_manager_api/OrderManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerApiApplication.class, args);
+	}
+
+}
